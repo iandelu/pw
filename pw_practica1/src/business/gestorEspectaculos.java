4b7eeed783aee06a9875c80a754a57d9a5f66920
Alejandro Rovira <71695107+alexrovira@users.noreply.github.com>
2021-10-07 13:51:43 +0200

    Actualizado gestorCriticas.java
    He añadido unas cuantas funciones, no hechas, solo lo que hay que hacer en cada funcion, xq hacerlas no se xd

--- pw_practica1/src/business/gestorEspectaculos.java
+++ pw_practica1/src/business/gestorEspectaculos.java
@@ -2,8 +2,10 @@ package business;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 import DAOS.DAOManager;
+import Data.Critica;
 import Data.Espectaculo;
 
 public class gestorEspectaculos {
@@ -21,20 +23,59 @@ public class gestorEspectaculos {
 		return instance;
 	}
 	
-	public void darAltaEspectaculo(String tipoEspectaculo, LocalDate fecha, LocalTime time) {
+	public void darAltaEspectaculo(String idEspectaculo, LocalDate fecha, LocalTime time) {
 	
-		Espectaculo nuevoEspectaculo = factoria.crearEspectaculo(tipoEspectaculo, fecha, time);
-		//añadir espectaculo a los ficheros
+		Espectaculo nuevoEspectaculo = factoria.crearEspectaculo(idEspectaculo, fecha, time);
+		//aÃ±adir espectaculo a los ficheros
 		
 	}
 	
-	public void cancelarEspectaculo(int idEspectaculo) {
+	public ArrayList<Espectaculo> consultarEspectaculos(){
+		return gestor.getEspectaculos().obtenerTodos();
+	}
+	
+	public Boolean cancelarEspectaculo(int idEspectaculo) 
+	{
+		for(int i=0;i<)//Crea una funcion que vaya guardando todos los espectaculos, la recorres con el for y si esta el id se borra.
 		
 	}
 	
-	public void actualizarDatos(int idEspectaculo) {
-		
+	public void actualizarDatos(String idEspectaculo, LocalDate fecha, LocalTime time) {
+		//Buscas el espectaculo en la lista creada, lo borras y metes uno nuevo con los datos que introduzca el usuario por el main
 	}
+	
+	public int contabilizarEntradas(int idEspectaculo) {
+		//Cuenta las entradas que tiene cada espectaculo. Como tenemos la variable privada localidadesRestantes, creamos una funcion que reste esta
+		//variable x si le compran x entradas, y el resto entre las entradas iniciales y localidadesRestantes son las vendidas.
+		//Devuelve el numero de entradas restantes
+	}
+	
+	public int compraEntrada(int idEspectaculo, int numeroEntradas) {
+		//restar numeroEntradas a idEspectaculo.
+	}
+	
+	public int localidadesRestantes(LocalDate fecha) {
+		//Funcion que al darle una fecha te devuelva las localidades restantes que quedan del espectaculo de ese dia
+	}
+	
+	public void busquedaTitulo(string tituloEspectaculo) {
+		//Introducimos un titulo y muestra da los datos de los espectaculos que tengan ese titulo
+	}
+	
+	public void busquedaCategoria(//Aqui va una categoria) {
+		//Introducimos una categoria y te da los datos de los espectaculos que sean de esa categoria
+	}
+
+	public void espectaculosConEntradas() {
+		//Buscar en la lista donde hemos guardado todos los espectaculos dados de alta, los que las localidadesrestantes>0, y mostrarlos en pantalla.
+	}
+	
+		//Faltan funciones del 9 al 12 por po er
+
+
+	
+	
+	
 	
 	
 }
