e4b7a4abcd372a1240b10cc4f1505e15c338526d
Alejandro Rovira <71695107+alexrovira@users.noreply.github.com>
2021-11-12 19:41:59 +0100

    Update Critica.java

--- pw_practica1/src/Data/Critica.java
+++ pw_practica1/src/Data/Critica.java
@@ -5,79 +5,79 @@ import java.util.ArrayList;
 
 public class Critica implements Serializable {
 
-	private String idCritica;
-	private String titulo;
-	private Usuario autor;
-	private int puntuacion;
-	private Espectaculo espectaculo;
-	private String texto;
-	private ArrayList<Valoracion> valoraciones;
-	
-	
-	public Critica(String idCritica, String titulo, Usuario autor, Espectaculo espectaculo, String texto, int puntuacion) {
-		super();
-		this.idCritica = idCritica;
-		this.titulo=titulo;
-		this.autor = autor;
-		this.espectaculo = espectaculo;
-		this.texto = texto;
-	}
-	public String getIdCritica() {
-		return idCritica;
-	}
-	public void setIdCritica(String idCritica) {
-		this.idCritica = idCritica;
-	}
-	public String getTituloCritica() {
-		return titulo;
-	}
-	public void setTituloCritica(String titulo) {
-		this.titulo = titulo;
-	}
-	public Usuario getAutor() {
-		return autor;
-	}
-	public void setAutor(Usuario autor) {
-		this.autor = autor;
-	}
-	public Espectaculo getEspectaculo() {
-		return espectaculo;
-	}
-	public void setEspectaculo(Espectaculo espectaculo) {
-		this.espectaculo = espectaculo;
-	}
-	public String getTexto() {
-		return texto;
-	}
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
-	public ArrayList<Valoracion> getValoraciones() {
-		return valoraciones;
-	}
-	public void setValoraciones(ArrayList<Valoracion> valoraciones) {
-		this.valoraciones = valoraciones;
-	}
-	
-	public void anadirValoracion(Valoracion nuevaValoracion) {
-		valoraciones.add(nuevaValoracion);
-	}
+    private String idCritica;
+    private String titulo;
+    private Usuario autor;
+    private int puntuacion;
+    private Espectaculo espectaculo;
+    private String texto;
+    private ArrayList<Valoracion> valoraciones;
+    
+    
+    public Critica(String idCritica, String titulo, Usuario autor, Espectaculo espectaculo, String texto, int puntuacion) {
+        super();
+        this.idCritica = idCritica;
+        this.titulo=titulo;
+        this.autor = autor;
+        this.espectaculo = espectaculo;
+        this.texto = texto;
+    }
+    public String getIdCritica() {
+        return idCritica;
+    }
+    public void setIdCritica(String idCritica) {
+        this.idCritica = idCritica;
+    }
+    public String getTituloCritica() {
+        return titulo;
+    }
+    public void setTituloCritica(String titulo) {
+        this.titulo = titulo;
+    }
+    public Usuario getAutor() {
+        return autor;
+    }
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+    public Espectaculo getEspectaculo() {
+        return espectaculo;
+    }
+    public void setEspectaculo(Espectaculo espectaculo) {
+        this.espectaculo = espectaculo;
+    }
+    public String getTexto() {
+        return texto;
+    }
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+    public ArrayList<Valoracion> getValoraciones() {
+        return valoraciones;
+    }
+    public void setValoraciones(ArrayList<Valoracion> valoraciones) {
+        this.valoraciones = valoraciones;
+    }
+    
+    public void adirValoracion(Valoracion nuevaValoracion) {
+        valoraciones.add(nuevaValoracion);
+    }
 
-	public int getPuntuacion() {
-		return puntuacion;
-	}
+    public int getPuntuacion() {
+        return puntuacion;
+    }
 
-	public void setPuntuacion(int puntuacion) {
-		this.puntuacion = puntuacion;
-	}
-	
-	@Override
-	public String toString() {
-		return "Critica [idCritica=" + idCritica + ", autor=" + autor + ", puntuacion=" + puntuacion + ", espectaculo="
-				+ espectaculo + ", texto=" + texto + ", valoraciones=" + valoraciones + "]";
-	}
-	
-	
-	
-	
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+    
+    @Override
+    public String toString() {
+        return "Critica [idCritica=" + idCritica + ", autor=" + autor + ", puntuacion=" + puntuacion + ", espectaculo="
+                + espectaculo + ", texto=" + texto + ", valoraciones=" + valoraciones + "]";
+    }
+    
+    
+    
+    
 }
