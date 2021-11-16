b19d2875f8c29e0c0c5c24fa974bd52225583304
Alejandro Rovira <71695107+alexrovira@users.noreply.github.com>
2021-11-16 01:46:40 +0100

    Create funciones_main.java

---
+++ pw_practica2/src/main/java/model/funciones_main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+import model.daos.*;
+import model.data.*;
+
+
+public class funciones_main {
+
+	private static MySQLDAOManager gestorDAO = new MySQLDAOManager();
+	
+	
+	public static Usuario iniciarSesion() {
+			
+		Usuario usuarioNew  = null;
+		while(usuarioNew == null) {
+			
+			System.out.println("---BIENVENIDO AL GESTOR DE CRITICAS---\n");
+			System.out.println("Introduce tu nickname: ");
+			Scanner entrada= new Scanner(System.in);
+			String nickname=entrada.nextLine();
+			
+			usuarioNew = gestorDAO.getUsuarios().obtener(nickname);
+		}
+		
+		return usuarioNew;
+	}
+	
+	public static int menu_iniciar_sesion()
+	{
+		boolean aux=false;
+		while(aux=false) {
+			System.out.println("Elija una de las 2 opciones siguientes:\n1:Iniciar sesion\n2:Registrarte\n");
+			Scanner entrada= new Scanner(System.in);
+			int opcion=entrada.nextInt();
+			
+			if(opcion<1 || opcion>2)
+			{
+				System.out.println("Inserte un numero correcto\n");
+			}
+			else {
+				aux=true;
+			}
+			return opcion;
+		}
+	}
+	
+	public static Usuario registrarse(String nickname) throws Exception {
+		
+
+		if(gestorDAO.getUsuarios().obtener(nickname) == null) {
+			
+			System.out.println("\nTe vas a registrar.: ");
+			
+			System.out.println("\nIntroduce tu nombre: ");
+			Scanner entrada= new Scanner(System.in);
+			String nombre=entrada.nextLine();
+			
+			System.out.println("\nIntroduce tus apellidos con guion intermedio(ejemplo: Fernandez_Jimenez): ");
+			entrada= new Scanner(System.in);
+			String apellidos=entrada.nextLine();
+			
+			System.out.println("\nIntroduce tu email: ");
+			entrada= new Scanner(System.in);
+			String emailnuevo=entrada.nextLine();
+			
+			System.out.println("\nIntroduce tu contraseña: ");
+			entrada = new Scanner(System.in);
+			String passwordRegister=entrada.nextLine();
+			
+			Usuario usuarioNew = new Usuario(nombre,apellidos,emailnuevo,nickname,passwordRegister);
+			gestorDAO.getUsuarios().insertar(usuarioNew);
+			return usuarioNew;
+		}
+		
+	}
+	
+	
+}
+
